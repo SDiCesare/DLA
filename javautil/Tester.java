a8a99b2bafbdc1585536fd6b706a80681136fd3f
SDiCesare <simone.dicesare.2001@gmail.com>
2024-02-17 02:33:34 +0100

    Exam Presentation

--- javautil/Tester.java
+++ javautil/Tester.java
@@ -18,10 +18,10 @@ public class Tester {
 
     public static void main(String[] args) {
         ArrayList<Test> tests = new ArrayList<>();
-        int sizes[] = {1000}; // 1000000
-        int particles[] = {256, 1024, 8192, 32768};
-        int steps[] = {100, 1000, 10000, 1000000};
-        String outDir = "./p_s";
+        int sizes[] = {100, 1000, 10000, 1000000}; // 1000000
+        int particles[] = {32768};
+        int steps[] = {10000};
+        String outDir = "./sizes";
         for (int size : sizes) {
             for (int particle : particles) {
                 for (int step : steps) {
@@ -38,11 +38,11 @@ public class Tester {
             Test test = tests.get(i);
             // ./PATH/main WIDTH HEIGHT PARTICLES STEPS CENTER_X CENTER_Y OUT_MAP > OUT.TXT
             System.out.printf("echo \"Test %d\"\n", i);
-            System.out.printf("echo \"Params: %d %d %d %d 99 99\"\n", test.width, test.height, test.particles, test.steps);
+            System.out.printf("echo \"Params: %d %d %d %d 0 0\"\n", test.width, test.height, test.particles, test.steps);
             for (String name : new String[]{"serial", "openmp", "cuda"}) {
                 System.out.printf("echo \"%s simulation\"\n", name);
                 String dir = "./bin/" + name + "_dla";
-                System.out.printf("%s %d %d %d %d 99 99 %s/%s_map_%d.txt > %s/%s_%d.txt\n", dir,
+                System.out.printf("%s %d %d %d %d 0 0 %s/%s_map_%d.txt > %s/%s_%d.txt\n", dir,
                     test.width, test.height, test.particles, test.steps,
                     outDir, name, i, outDir, name, i);
             }
